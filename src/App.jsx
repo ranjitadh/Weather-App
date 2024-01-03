6309467b514aa419aed4af01de0c4d4c9e2d34aa
Ranjit Adhikari <102475302+ranjitadh@users.noreply.github.com>
2024-01-03 20:56:23 +0545

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.css";
+import { useState } from "react";
+
+const api = {
+  key: "949da85479f58546e2f48b54b4cfd98a",
+  url: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  // search buttton pressed
+  const searchPressed = () => {
+    fetch(`${api.url}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+
+        {/* Search Box ,Input and Button  */}
+        <div>
+          <input
+            type="text"
+            placeholder="Enter city/town..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchPressed}>Search</button>
+        </div>
+
+        {/* If weather is not undefined display results from API */}
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            {/* Location  */}
+            <p>{weather.name}</p>
+
+            {/* Temperature Celsius  */}
+            <p>{weather.main.temp}°C</p>
+
+            {/* Condition (Sunny ) */}
+            <p>{weather.weather[0].main}</p>
+            <p>({weather.weather[0].description})</p>
+          </div>
+        ) : (
+         // <p>{console.log("City Not Found")}</p>
+       "")}
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
